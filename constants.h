d7da7d8f498bf52f966cf2d378e78371b0b9407f
Stiti-create <stitisahoo18@gmail.com>
2024-02-03 21:57:02 +0530

    random generator corrected

--- constants.h
+++ constants.h
@@ -1,4 +1,4 @@
-#define N 6
+#define N 10
 #define INPUT_MATRIX_FILE "matrix.txt"
 #define DEBUG_OUT_FILE "debug.txt"
 #define DEBUG 1
--- matrix_gen.cpp
+++ matrix_gen.cpp
@@ -1,19 +1,31 @@
 #include <bits/stdc++.h>
 #include <fstream>
 #include "constants.h"
+#include <random>
 using namespace std;
 
 int main(){
+
     ofstream fout;
     fout.open(INPUT_MATRIX_FILE);
+    if(!fout.is_open()){
+        perror("Error opening file");
+        return 0;
+    }
+
     vector<vector<double>> A(N, vector<double>(N, 0.0));
+    random_device rd;
+    default_random_engine generator(rd());
+    uniform_real_distribution<double> distribution(0.0, 1.0);
+
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
-            A[i][j] = drand48();
+            A[i][j] = distribution(generator);
             fout << A[i][j] << " ";
         }
         fout << endl;
     }
+    
     fout.close();
     return 0;
 }
